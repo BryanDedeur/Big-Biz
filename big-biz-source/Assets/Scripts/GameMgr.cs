bd1e73d8668a78bae4b8b88d061cec49c3179a26
BryanDedeur <bdedeurwaerder@nevada.unr.edu>
2019-04-26 13:25:29 -0700

    release current vr work

---
+++ big-biz-source/Assets/Scripts/GameMgr.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Author: 	Bryan Dedeurwaerder
+ * Date: 	Mar 18. 2019
+ * Email: 	bdedeurwaerder@nevada.unr.edu
+ * Web: 	https://github.com/BryanDedeur
+ * Project:	Assignment 5 - Converted into Unity
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Component = System.ComponentModel.Component;
+
+public class GameMgr : Mgr
+{
+    public int PrefabSpacing;
+    
+    private PrefabMgr prefabMgrScript;
+    
+    // Awake (init) for refrences between scripts, initializations. Runs first even when not enabled
+    void Awake()
+    {
+        
+    }
+    
+    
+    // Start (loadLevel) is called before the first frame update only when enabled
+    void Start()
+    {
+        // gets the prefab manager
+        prefabMgrScript = (PrefabMgr) gameObject.GetComponent(typeof(PrefabMgr));
+
+        // ------------------ SPAWN GAME ASSETS NOT ALREADY IN SCENE -----------------------//
+        // set default position and orientation
+        Vector3 spawnPos = new Vector3((float)(-PrefabSpacing * (5 - 1) / 2f ),-2,0);
+        Quaternion defaultOrientation = new Quaternion();
+
+        prefabMgrScript.SpawnPrefabOfTypeAtPosition(prefabMgrScript.Battleship, spawnPos, defaultOrientation);
+        spawnPos.x += PrefabSpacing;
+        prefabMgrScript.SpawnPrefabOfTypeAtPosition(prefabMgrScript.Corvette, spawnPos, defaultOrientation);
+        spawnPos.x += PrefabSpacing;
+        prefabMgrScript.SpawnPrefabOfTypeAtPosition(prefabMgrScript.Cruiser, spawnPos, defaultOrientation);
+        spawnPos.x += PrefabSpacing;
+        prefabMgrScript.SpawnPrefabOfTypeAtPosition(prefabMgrScript.Destroyer, spawnPos, defaultOrientation);
+        spawnPos.x += PrefabSpacing;
+        prefabMgrScript.SpawnPrefabOfTypeAtPosition(prefabMgrScript.Frigate, spawnPos, defaultOrientation);
+
+    }
+
+    // OnApplicationQuit (stop) is called when the application is shutting down
+    void OnApplicationQuit()
+    {
+        
+    }
+
+    // Update (tick) is called once per frame. 
+    void Update()
+    {
+        
+    }
+}
