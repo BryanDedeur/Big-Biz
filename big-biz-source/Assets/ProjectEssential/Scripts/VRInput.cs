851217e24b0584eafb26e435bd506c122e0092af
BryanDedeur <bdedeurwaerder@nevada.unr.edu>
2019-04-28 18:03:27 -0700

    release some vr updates

---
+++ big-biz-source/Assets/ProjectEssential/Scripts/VRInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VRInput : MonoBehaviour
+{
+    public GameObject test;
+
+    public SteamVR_Action_Boolean m_BooleanAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_BooleanAction[SteamVR_Input_Sources.LeftHand].stateDown)
+        {
+            Debug.Log("OKAy");
+        }
+    }
+}
